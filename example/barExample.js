9a722e536c55b3361bee1f115bc957451848d2f8
zhaoyuxiang <112408584@qq.com>
2017-03-28 15:19:37 +0800

    场景设置，摇色子做二维底，次数为数值

--- example/barExample.js
+++ example/barExample.js
@@ -4,7 +4,27 @@
 window.onload = function () {
   var dom = document.getElementById('thiCharts');
 
-  console.log(dom);
   var chart = new ThiCharts(dom);
-  console.log(chart);
+  //情景设定：同时摇两个筛子1W次，A筛子的要中数值为X轴，B筛子要中的数值为Z轴，摇中相应数值的次数为Z轴
+  var opt = {
+    type: 'bar',
+    data: [
+      [1,2,3,4,5,6],
+      [1,2,3,4,5,6],
+      [1,2,3,4,5,6],
+      [1,2,3,4,5,6],
+      [1,2,3,4,5,6],
+      [1,2,3,4,5,6]
+    ],
+    axisUnit: {
+      x: '',
+      y: '次',
+      z: ''
+    },
+    axisValue:{
+      x: ['1', '2', '3' ,'4', '5', '6'],
+      z: ['1', '2', '3' ,'4', '5', '6']
+    }
+  };
+  chart.setOption(opt);
 };
