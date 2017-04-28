87f9fa9c33748e49e977ed1ca9de6567b0acc74c
zhaoyuxiang <112408584@qq.com>
2017-04-28 16:08:57 +0800

    柱状图初步测试级动画测试

--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,30 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    home:'./src/thiCharts.js',
-    example: './example/barExample.js'
+    home: './src/thiCharts.js',
+    lib: './lib/controls/TrackballControls.js',
+    example: './example/barExample.js',
+    trackballExample: './example/trackball.js'
   },
   output: {
-    path: path.resolve(__dirname,'./dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
-    publicPath: ''
+    publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'three.js demo',
       template: 'index.html',
       filename: 'index.html',
-      chunks: ['example','home'],
+      chunks: ['example', 'home', 'lib'],
       inject: 'head'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'three.js demo',
+      template: './entry/index.html',
+      filename: 'trackballControls.html',
+      chunks: ['trackballExample'],
+      inject: 'body'
     })
   ]
 };
